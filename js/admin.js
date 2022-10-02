8a0f39938e3e3f034f8d0b05ac73b8bd002d4196
david serna <d_9033@hotmail.com>
2022-10-02 18:19:37 -0500

    cambios varios - iniciando  listar todas las plantas en agregar plantas

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,48 @@
 const nombreUsuario = document.querySelector("#nombreUsuario") 
 const linkCerrarSesion = document.querySelector("#linkCerrarSesion")
+const urlListaPlantas = "http://localhost:8080/api/v1/Plant/Plants"
+const btnPlantaEstadoSelecionadaOpciones = document.querySelector(".planta_estado_selecionada_opciones")
+const btnPlantaEstadoSelecionadaVer = document.querySelector(".planta_estado_selecionada_ver")
+const btnPlantaEstadoSelecionadaQuitar = document.querySelector(".planta_estado_selecionada_quitar")
+const btnPlantaEstadoSelecionadaMas = document.querySelector(".planta_estado_selecionada_mas")
+const modalAgregarPlanta = document.querySelector(".modal_agregar_planta")
+const btnModalAgregarPlantaConfirmar = document.querySelector("#modal_agregar_planta_btn_confirmar");
+const btnModalAgregarPlantaCancelar = document.querySelector('#modal_agregar_planta_btn_cancelar');
+
+
+
+
+btnModalAgregarPlantaCancelar.addEventListener("click",()=>{
+    modalAgregarPlanta.classList.toggle('mostrarf')
+    window.onscroll = "";
+    btnPlantaEstadoSelecionadaVer.classList.toggle('reset-transform')
+    btnPlantaEstadoSelecionadaQuitar.classList.toggle('reset-transform')
+    btnPlantaEstadoSelecionadaMas.classList.toggle('reset-transform')
+    
+})
+
+
+
+
+
+btnPlantaEstadoSelecionadaOpciones.addEventListener("click",()=>{
+
+    btnPlantaEstadoSelecionadaVer.classList.toggle('reset-transform')
+    btnPlantaEstadoSelecionadaQuitar.classList.toggle('reset-transform')
+    btnPlantaEstadoSelecionadaMas.classList.toggle('reset-transform')
+
+})
+
+
+btnPlantaEstadoSelecionadaMas.addEventListener('click',()=>{
+
+    /* console.log("clik en mas") */
+    modalAgregarPlanta.classList.toggle('mostrarf')
+    window.scrollTo(0,0)
+    window.onscroll = () => { window.scroll(0, 0); };
+
+}) 
+
 
 if(localStorage.getItem('userName')){
     nombreUsuario.innerHTML = localStorage.getItem('userName')
@@ -7,10 +50,31 @@ if(localStorage.getItem('userName')){
     window.location.href="login.html";
 }
 
-
-
 linkCerrarSesion.addEventListener("click",()=>{
     localStorage.removeItem('login')
     localStorage.removeItem('userName')
     window.location.href="index.html"
 })
+
+btnPlantaEstadoSelecionadaOpciones.addEventListener("click",()=>{
+
+})
+
+async function listaPlantas(){
+
+    await fetch(urlListaPlantas,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }   
+    })
+    .then((response) => response.json())
+    .then((data)=> {
+        console.log(data.data)
+    })
+    .catch((err) => console.log(err))
+
+}
+
+listaPlantas()
